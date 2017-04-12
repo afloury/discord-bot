ffdc444688c96aa24e2b1a578329ffc2bec5347e
afloury <antoine.floury@imerir.com>
2017-04-12 13:57:11 +0200

    update time remain

--- commands/time/time_remain.js
+++ commands/time/time_remain.js
@@ -10,29 +10,23 @@ class TimeCommand extends commando.Command {
     }
 
     async run(message, args) {
-        var d = new Date()
-        var hours = d.getHours()
-        var min = d.getMinutes()
-        var sec = d.getSeconds()
-        if (hours > 17 || (hours == 17 && min >= 30) || hours < 8 || (hours == 8 && min < 15)) {
-            message.reply("La journée est déjà fini bolosse !")
-        } else {
-            var hoursDisp = 17 - hours
-            console.log("hoursDisp = " + hoursDisp + " donc 10 - " + hours)
-            if (min > 0) {
-                hoursDisp = hoursDisp - 1
-                console.log("hoursDisp = " + hoursDisp + " donc 10 - " + hours + " -1")
-            }
-            var minDisp = 0
-            if (min < 30) {
-                minDisp = min + 30
-            } else if (min >= 30) {
-                var temp = 60 - min
-                minDisp = temp + 30
-            }
-            message.reply("Il reste " + hoursDisp + "heures" + " et " + minDisp + "min avant la fin de la journée !")
+        var current = new Date()
+        var endDate = new Date()
+        endDate.setHours(17, 30)
+
+        var startDate = new Date()
+        startDate.setHours(8, 30)
+
+        var timeRemaining = new Date(endDate - current)
+        console.log("date actuelle ->" + current)
+
+        if (timeRemaining > 0) {
+            console.log("Time remain ->" + timeRemaining)
+            message.reply("Il reste " + timeRemaining.getUTCHours() + "heures" + " et " + timeRemaining.getUTCMinutes() + "min avant la fin de la journée !")
         }
-        
+        else {
+            message.reply("La journée est déjà fini bolosse !")
+        }
     }
 }
 
